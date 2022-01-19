a7ff1d0659f451c07bbb888bf453862157853cdf
Nikolay Kalchev <nikolay.kalchev@abv.bg>
2022-01-19 19:23:05 +0200

    added setup files and a log handler

--- routes/mux.go
+++ routes/mux.go
@@ -13,5 +13,7 @@ func Mux(log *log.Logger, cfg *config.Config) *http.ServeMux {
 	index := http.FileServer(http.Dir(cfg.IndexDir))
 	mux.Handle("/", index)
 
+    mux.Handle("/it_remains.log", http.FileServer(http.Dir("./logs/")))
+
 	return mux
 }
