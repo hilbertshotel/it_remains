13f71fe66219156e7cb80bdf63585b6c2b4b6a2f
hilbertshotel <nikolay.kalchev@abv.bg>
2022-01-27 15:11:54 +0200

    returned steps to previous style

--- frontend/js/elements/steps.js
+++ frontend/js/elements/steps.js
@@ -1,44 +1,17 @@
 
 const initSteps = () => {
 
-  let
-    noSteps = get("steps1"),
-    firstStep = get("steps2"),
-    leftStep = get("steps3"),
-    rightStep = get("steps4"),
-    // steps = [get("steps3"), get("steps4")],
-    // last = steps.length-1,
-    pos = 0
+  const steps = [get("steps1"), get("steps2"), get("steps3"), get("steps4")]
+  let pos = 0
 
   return {
 
     move: () => {
-
-      if (pos === 0 ) {
-        noSteps.style.visibility = "hidden"
-        firstStep.style.visibility = "visible"
-        pos++
-      }
-
-      else if (pos === 1) {
-        firstStep.style.visibility = "hidden"
-        leftStep.style.visibility = "visible"
-        pos++
-      }
-
-      else if (pos === 2) {
-        leftStep.style.visibility = "hidden"
-        rightStep.style.visibility = "visible"
-        pos++
-      }
-
-      else {
-        rightStep.style.visibility = "hidden"
-        leftStep.style.visibility = "visible"
-        pos--
-      }
-      
+      steps[pos].style.visibility = "hidden"
+      pos = pos === 3 ? 0 : pos+1
+      steps[pos].style.visibility = "visible"
     }
 
   }
+
 }
